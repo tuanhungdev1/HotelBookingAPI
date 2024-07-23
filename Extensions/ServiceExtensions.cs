5b48b1f5ca8f16caaa836badbc2aa361930ad9a5
tuanhungdev1 <tuanhungdev274@gmail.com>
2024-07-23 21:18:59 +0700

    Connect SQL Server

---
+++ Extensions/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using HotelBookingAPI.Infrastructure.SQLServer.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingAPI.Infrastructure.Extensions {
+    public static class ServiceExtensions {
+
+
+        public static void ConfigureSQLContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
+using HotelBookingAPI.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureSQLContext(builder.Configuration);
 
 var app = builder.Build();
 
